356a737c7f32d12a094b7517b5534ef802ae1fe1
Danny <liy37@rpi.edu>
2018-05-01 17:27:27 -0400

    process name change

--- process.cpp
+++ process.cpp
@@ -6,7 +6,7 @@ Process::Process() {}
 Process::Process(char _name, int _frames, std::vector<int> _arrTimes, std::vector<int> _runTimes) {
   name = _name;
   frames = _frames;
-  arrivalTimes = _arrTimes;
+  arrTimes = _arrTimes;
   runTimes = _runTimes;
 
 }
--- process.h
+++ process.h
@@ -9,7 +9,7 @@ class Process {
   Process(char _name, int _frames, std::vector<int> _arrTimes, std::vector<int> _runTimes);
   char name;
   int frames;
-  std::vector<int> arrivalTimes;
+  std::vector<int> arrTimes;
   std::vector<int> runTimes;
 };
 #endif
