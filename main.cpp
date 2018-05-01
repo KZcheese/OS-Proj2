ba6f9ff026dbfd6c98680409230dc7720d036b4f
Danny <liy37@rpi.edu>
2018-05-01 17:02:24 -0400

    error handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <stdio.h>
 
-int main() {
+int main(int argc, char * argv[]) {
+    if (argc != 2){
+        fprintf(stderr, "%s\n%s\n", "ERROR: Invalid arguments",
+                "USAGE: ./a.out <input-file> ");
+        return EXIT_FAILURE;
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
