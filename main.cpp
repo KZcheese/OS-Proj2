2fdaa8ecdad03be7dc48f78ba0e7d56ea8d37234
Danny <liy37@rpi.edu>
2018-05-01 19:13:56 -0400

    started nonConntiguous

--- main.cpp
+++ main.cpp
@@ -18,7 +18,16 @@ void nextFit() {}
 void bestFit() {}
 // you can just not use this if you dont want to
 void worstFit() {}
-void nonContiguous() {}
+void nonContiguous(std::vector<Process> process) {
+  int ms = 0;
+  int freeSpaces = 256;
+  std::vector<char> memory;
+  for (int i = 0; i < 256; ++i) {
+    memory.push_back('.');
+  }
+  std::cout << "time " << ms << "ms: Simulator started (Non-contiguous)"
+            << std::endl;
+}
 
 int main(int argc, char * argv[]) {
     if (argc != 2){
