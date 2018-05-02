082c9278c6425ef04f209d183e97907c96e67519
Kevin Zhan <kevin.w.zhan@gmail.com>
2018-05-01 21:29:31 -0400

    worst fit draft

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <climits>
+#include <algorithm>
 
 
 const int frames = 256;
 const int fPerLine = 32;
+const int t_memmove = 1;
 
 std::vector<Process> processes;
 
@@ -25,58 +28,111 @@ void printMemory(std::vector<char> memory) {
     std::cout << "================================" << std::endl;
 }
 
+void nextFit() {}
+
+void bestFit() {}
+
 void writeMem(std::vector<char> &memory, const Process &p, const bool &add) {
     char writeChar;
     if (add) writeChar = p.name;
     else writeChar = '.';
     for (int i = 0; i < p.frames; i++)
         memory[i + p.location] = writeChar;
-
 }
 
-void nextFit() {}
+bool compByLoc(Process i, Process j){
+    return i.location < j.location;
+}
 
-void bestFit() {}
+std::pair<int, int> defrag(std::vector<char> &memory, std::vector<Process> &active, int &offset) {
+    std::sort(memory.begin(), memory.end(), compByLoc);
+    int end = 0;
+    for (int i = 0; i < active.size(); i++) {
+        active[i].location = end;
+        end = active[i].location + active[i].frames;
+    }
+    return std::pair<int, int>{end, frames - end};
+}
 
 void worstFit() {
-    int clock = 0;
+    int clock = 0, offset = 0;
     std::vector<char> memory(frames, '.');
     std::vector<Process> inactive(processes);
     std::vector<Process> active;
     std::vector<std::pair<int, int> > gaps;
-    gaps.push_back(std::pair<int, int>(0, frames));
 
+    int spaceUsed = 0;
+    int jumpTime = INT_MAX;
+    while (!active.empty() && !inactive.empty()) {
 
-    int maxGapSize = 0;
-    int maxGap = -1;
-    while (true) {
         for (int i = 0; i < active.size(); i++) {
             Process p = active[i];
-            if (p.arrTimes[p.burst] + p.runTimes[p.burst] <= clock) {
+
+            if (p.arrTimes[p.burst] + p.runTimes[p.burst] + offset <= clock) {
+                writeMem(memory, p, false);
+                active.erase(active.begin() + i--);
+
                 p.burst++;
                 p.location = -1;
-                active.erase(active.begin() + i--);
-                if (p.burst < p.arrTimes.size() - 1)
+                spaceUsed += p.frames;
+
+                if (p.burst < p.arrTimes.size() - 1) {
+                    if (jumpTime > p.arrTimes[p.burst] + offset) jumpTime = p.arrTimes[p.burst] + offset;
                     inactive.push_back(p);
+                }
+            }
+        }
+        int maxGapSize = 0;
+        int maxGapLoc = -1;
+        for (int i = 0; i < frames; i++) {
+            if (memory[i] == '.') {
+                int j = i + 1;
+
+                for (; j < frames; j++) if (memory[j] != '.') break;
+
+                gaps.push_back(std::pair<int, int>(i, j - i));
+
+                if (j - i > maxGapSize) {
+                    maxGapSize = j - i;
+                    maxGapLoc = gaps.size() - 1;
+                }
+
+                i = j;
             }
         }
 
-        std::vector<Process> loadBuff;
-        for (Process p: inactive) {
-            if (p.arrTimes[p.burst] <= clock) {
-                loadBuff.push_back(p);
-            }
+        for (int i = 0; i < inactive.size(); i++) {
+            Process p = inactive[i];
+            if (p.arrTimes[p.burst] + offset <= clock) {
 
+                if (maxGapSize < p.frames) {
+                    if (frames - spaceUsed >= p.frames) {
+                        std::pair<int, int> gap = defrag(memory, active, offset);
+                        maxGapLoc = gap.first;
+                        maxGapSize = gap.second;
+                    } else {
+                        inactive[i].burst++;
+                        continue;
+                    }
+                }
+
+                p.location = gaps[maxGapLoc].first;
+
+                spaceUsed -= p.frames;
+                writeMem(memory, p, true);
+                inactive.erase(inactive.begin() + i--);
+                active.push_back(p);
+
+                if (maxGapSize < p.frames) {
+                    gaps[maxGapLoc].first = p.location + p.frames;
+                    gaps[maxGapLoc].second -= p.frames;
+                } else gaps.erase(gaps.begin() + maxGapLoc);
+
+                if (p.runTimes[p.burst] + offset < jumpTime) jumpTime = p.runTimes[p.burst] + offset;
+            } else if (p.arrTimes[p.burst] + offset < jumpTime) jumpTime = p.arrTimes[p.burst] + offset;
         }
-        for (std::pair<int, int> gap : gaps) {
-            if (gap.second > maxGapSize) {
-                maxGapSize = gap.second;
-                maxGap = gap.first;
-            }
-            if (maxGapSize >= frames) break;
-        }
-        clock++;
-        break;
+
+        clock = jumpTime;
     }
 }
 
