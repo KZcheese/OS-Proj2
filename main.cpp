26c5a91e78ffc7e5ea7de7890feebb97658b8ae3
Danny <liy37@rpi.edu>
2018-05-01 21:27:11 -0400

    non contig only fails test 2

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ std::vector<Process> processes;
 void printMemory(std::vector<char> memory) {
     std::cout << "================================" << std::endl;
     for (int i = 0; i < frames; i++) {
-        if (i % fPerLine == 0) {
+        if (i % fPerLine == 0 && i != 0) {
             std::cout << std::endl;
         }
         std::cout << memory[i];
@@ -26,7 +26,28 @@ void printMemory(std::vector<char> memory) {
 }
 
 void printPageTable(std::vector<char> memory, std::vector<Process> process){
-
+  	unsigned int counter = 0;
+	std::cout << "PAGE TABLE [page,frame]:\n";
+        for (unsigned int i = 0; i < process.size(); i++) {
+          if (!process[i].positions.empty()) {
+            std::cout << process[i].name << ": ";
+            for (unsigned int j = 0; j < process[i].positions.size(); j++) {
+              if (counter == process[i].positions.size() - 1) {
+                std::cout << "[" << counter << "," << process[i].positions[j]
+                          << "]" << std::endl;
+                break;
+              } else if (counter % 10 == 9 && counter != 0) {
+                std::cout << "[" << counter << "," << process[i].positions[j]
+                          << "]" << std::endl;
+              } else {
+                std::cout << "[" << counter << "," << process[i].positions[j]
+                          << "] ";
+              }
+              counter++;
+            }
+            counter = 0;
+        }
+	}
 }
 void writeMem(std::vector<char> &memory, const Process &p, const bool &add) {
     char writeChar;
@@ -84,55 +105,79 @@ void worstFit() {
 }
 
 void nonContiguous() {
-  int ms = 0;
-  int freeSpaces = frames;
-  std::vector<char> memory(frames, '.');
-  std::cout << "time " << ms << "ms: Simulator started (Non-contiguous)"
-            << std::endl;
-  std::vector<Process> newProcess(processes);
-  while (!newProcess.empty()) {
-    for (unsigned int i = 0; i < processes.size(); i++) {
-      if (ms == processes[i].arrTimes[0]) {
-        std::cout << "time " << ms << "ms: Process " << processes[i].name
-                  << " arrived (requires " << processes[i].frames << " frames)"
-                  << std::endl;
+	int ms = 0;
+	int freeSpaces = frames;
+	std::vector<char> memory(frames, '.');
+	std::cout << "time " << ms << "ms: Simulator started (Non-contiguous)"
+				<< std::endl;
+	std::vector<Process> newProcess(processes);
+	while (!newProcess.empty()) {
+		for (unsigned int i = 0; i < newProcess.size(); i++) {
+			if (newProcess[i].runTimes[0] == ms - newProcess[i].arrTimes[0]) {
+				std::cout << "time " << ms << "ms: Process " << newProcess[i].name
+						<< " removed:" << std::endl;
+				for (int j = 0; j < 256; j++) {
+					if (memory[j] == newProcess[i].name) {
+						memory[j] = '.';
+					}
+				}
+				newProcess[i].runTimes.erase(newProcess[i].runTimes.begin());
+				newProcess[i].arrTimes.erase(newProcess[i].arrTimes.begin());
+				freeSpaces += newProcess[i].frames;
+				newProcess[i].positions.clear();
+				printMemory(memory);
+				if (newProcess[i].runTimes.empty() ||
+					newProcess[i].arrTimes.empty()) {
+				newProcess.erase(newProcess.begin() + i);
+				}
+				printPageTable(memory, newProcess);
+			}
+		}
+		for (unsigned int i = 0; i < newProcess.size(); i++) {
+			if (ms == newProcess[i].arrTimes[0]) {
+			std::cout << "time " << ms << "ms: Process "
+						<< newProcess[i].name << " arrived (requires "
+						<< newProcess[i].frames << " frames)"
+						<< std::endl;
 
-        // where we add to the processes coordinates
-        // and where we change the board
-        if (freeSpaces < processes[i].frames) {
-          std::cout << "time " << ms << "ms: Cannot place process "
-                    << processes[i].name << " -- skipped!" << std::endl;
-          processes[i].runTimes.erase(processes[i].runTimes.begin());
-          processes[i].arrTimes.erase(processes[i].arrTimes.begin());
-          if (processes[i].runTimes.empty()) {
-            processes.erase(processes.begin() + i);
-            i--;
-          }
-          printMemory(memory);
-          printPageTable(memory, processes);
-          continue;
-        }
+			if (freeSpaces < newProcess[i].frames) {
+				std::cout << "time " << ms << "ms: Cannot place process "
+						<< newProcess[i].name << " -- skipped!"
+						<< std::endl;
+				newProcess[i].runTimes.erase(
+					newProcess[i].runTimes.begin());
+				newProcess[i].arrTimes.erase(
+					newProcess[i].arrTimes.begin());
+				if (newProcess[i].runTimes.empty()) {
+				newProcess.erase(newProcess.begin() + i);
+				i--;
+				}
+				continue;
+			}
 
-        if (freeSpaces >= processes[i].frames) {
-          int counter = 0;
-          for (int x = 0; x < frames; ++x) {
-            if (memory[x] == '.' && counter < processes[i].frames) {
-              processes[i].positions.push_back(x);
-              memory[x] = processes[i].name;
-              counter++;
-            }
-          }
-          std::cout << "time " << ms << "ms: Placed process "
-                    << processes[i].name << ":" << std::endl;
-          freeSpaces -= processes[i].frames;
-        }
-        printMemory(memory);
-        // printPageTable(memory, processes);
-        continue;
-      }
-    }
-  }
-}
+			if (freeSpaces >= newProcess[i].frames) {
+				int counter = 0;
+				for (int x = 0; x < frames; ++x) {
+				if (memory[x] == '.' &&
+					counter < newProcess[i].frames) {
+					newProcess[i].positions.push_back(x);
+					memory[x] = newProcess[i].name;
+					counter++;
+				}
+				}
+				std::cout << "time " << ms << "ms: Placed process "
+						<< newProcess[i].name << ":" << std::endl;
+				freeSpaces -= newProcess[i].frames;
+			}
+			printMemory(memory);
+			printPageTable(memory, newProcess);
+			continue;
+			}
+		}
+		ms++;
+	}
+	std::cout << "time " << ms-1 << "ms: Simulator ended (Non-contiguous)" << std::endl;
+	}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -164,6 +209,7 @@ int main(int argc, char *argv[]) {
         processes.push_back(Process(id, size, arrTimes, runTimes));
     }
     in.close();
-    return 0;
+    nonContiguous();
+    return 1;
 
 }
